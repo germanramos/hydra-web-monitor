824bff164175a55aa13be8ce3d430cd636d4eda4
Alberto Gregoris <alberto.gregoris@intelygenz.com>
2014-09-01 16:31:41 +0200

    Fixed default iframe address

--- app/config/Config.js
+++ app/config/Config.js
@@ -4,7 +4,7 @@ Ext.define('HydraWM.config.Config',{
         hydraServerAddr: 'localhost',
         hydraServerAdminPort: '7771',
         hydraServerEtcdPort: '4001',
-        iframeAddr: 'http://www.marca.com',
+        iframeAddr: 'http://localhost:4001/mod/dashboard',
         probePassword: '',
         hydraProbePort: '9099'
     }
